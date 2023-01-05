3840a17a69fe60529e47bb59050502c3b1a94827
William <sidiwi9@gmail.com>
2023-01-04 23:42:46 -0300

    Implementado sistema de request por meio de entrada de número do paciente, retornando informações como sexo e nome, além do seu histórico de atendimentos.

--- frontend/pacientes2003/src/Components/Header/Header.tsx
+++ frontend/pacientes2003/src/Components/Header/Header.tsx
@@ -1,26 +1,42 @@
-import react from "react"
-import componentDidMount from "react"
-
 import axios from "axios"
+import { useState } from "react"
 
+// Código crasha quando não encontra o paciente
 
 export const Header = () => {
-  let patientInfo = ''
+  let patientInfo = {}
+  const [patient, setPatient] = useState({
+    "Nome": '',
+    "IDPaciente": '',
+    "SobreNome": '',
+    "Telefone": '',
+    "Sexo": '',
+    "HistoricoMorbido": ''
   
-  
-  componentDidMount() {
-    const requestApi = async (id: number) => {
-      const response = await axios.get('http://localhost:3001/api/Pacientes?IDPaciente=' + id)
-      patientInfo = response.data
-      return response.data
-    }
-  }
-  
+  })
 
+  
+  const requestApi = async (id: number) => {
+    const response = await axios.get('http://localhost:3001/api/Pacientes?IDPaciente=' + id)
+    // const {Nome, IDPaciente, SobreNome} = response.data[0]
+    setPatient(response.data[0]);
+  }
     return(
       <>
-        <h1>Teste</h1>
-        {patientInfo}
+        <input type="text" defaultValue={1} onChange={(e) => {
+          if(e.target.value !== "") {
+            requestApi(parseInt(e.target.value))
+          } else{
+            requestApi(1)
+          }
+        }} />
+
+        <h1>{(patient.Nome + ' ' + patient.SobreNome).toUpperCase()}</h1>
+        <h3>Sexo: {patient.Sexo.toUpperCase() === "M" ? "Homem" : "Mulher"}</h3>
+        <h2>Número de Telefone: {patient.Telefone}</h2>
+        <p>
+          <strong>Histórico: </strong>{patient.HistoricoMorbido}
+        </p>
       </>
     )
 }
